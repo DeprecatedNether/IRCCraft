3b9ac6583f043e1121e68c462cf8a83c05a292f1
DeprecatedNether <deprecatednether@gmail.com>
2014-06-30 17:58:27 +0200

    Add event listeners that send messages to IRC

---
+++ src/main/java/net/unknownmc/irc/Listeners.java
@@ -0,0 +1,42 @@
+package net.unknownmc.irc;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class Listeners implements Listener {
+
+    IRCCraft main;
+
+    public Listeners(IRCCraft main) {
+        this.main = main;
+    }
+
+    @EventHandler
+    public void join(PlayerJoinEvent e) {
+        if (!main.getConfig().getBoolean("actions.join")) return;
+        main.bot.sendMessageToIRC("[JOIN] " + e.getPlayer().getName());
+    }
+
+    @EventHandler
+    public void leave(PlayerQuitEvent e) {
+        if (!main.getConfig().getBoolean("actions.quit")) return;
+        main.bot.sendMessageToIRC("[QUIT] " + e.getPlayer().getName());
+    }
+
+    @EventHandler
+    public void kick(PlayerKickEvent e) {
+        if (!main.getConfig().getBoolean("actions.kick")) return;
+        main.bot.sendMessageToIRC("[KICK] " + e.getPlayer().getName() + ": " + ChatColor.stripColor(e.getReason().replace("\n", "")));
+    }
+
+    @EventHandler
+    public void chat(AsyncPlayerChatEvent e) {
+        if (!main.getConfig().getBoolean("actions.chat") || e.isCancelled()) return;
+        main.bot.sendMessageToIRC("[CHAT] " + e.getPlayer().getName() + ": " + ChatColor.stripColor(e.getMessage()));
+    }
+}
