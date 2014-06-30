e44c758993519e2dbe3d8c1e42af51bd92c7992a
DeprecatedNether <deprecatednether@gmail.com>
2014-06-30 17:45:17 +0200

    Run the IRC bouncer asynchronously

--- src/main/java/net/unknownmc/irc/IRCCraft.java
+++ src/main/java/net/unknownmc/irc/IRCCraft.java
@@ -3,8 +3,11 @@ package net.unknownmc.irc;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class IRCCraft extends JavaPlugin {
-    public void onEnable() {
+    public IRCBot bot;
 
+    public void onEnable() {
+        this.bot = new IRCBot(this, getConfig().getString("irc.hostname"), getConfig().getInt("irc.port"), getConfig().getString("irc.password"), getConfig().getString("irc.channel"));
+        this.bot.runTaskAsynchronously(this);
     }
 
     public void onDisable() {
