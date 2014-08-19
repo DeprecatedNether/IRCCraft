393f918ab25004b2c6883d222cbf170b6174bcfc
DeprecatedNether <deprecatednether@gmail.com>
2014-08-19 10:40:07 +0200

    Don't try joining until we receive the end of MOTD

--- src/main/java/net/unknownmc/irc/IRCBot.java
+++ src/main/java/net/unknownmc/irc/IRCBot.java
@@ -33,8 +33,6 @@ public class IRCBot extends BukkitRunnable {
                 this.writer.write("PRIVMSG ChanServ :INVITE " + channel + "\r\n");
                 this.writer.flush();
             }
-            this.writer.write("JOIN " + channel + "\r\n");
-            this.writer.flush();
             this.connected = true;
             // We have identified and may now start the loop in run()
         } catch (IOException ioe) {
@@ -70,10 +68,15 @@ public class IRCBot extends BukkitRunnable {
         }
         String ln;
         try {
+            boolean joined = false;
             while ((ln = this.reader.readLine()) != null && this.connected) {
                 if (ln.toLowerCase().startsWith("ping")) { // Respond to pings
                     this.writer.write("PONG " + ln.substring(5) + "\r\n");
                     this.writer.flush();
+                } else if (!joined && ln.contains("376")) {
+                    this.writer.write("JOIN " + channel + "\r\n");
+                    this.writer.flush();
+                    joined = true;
                 }
             }
         } catch (IOException ioe) {
